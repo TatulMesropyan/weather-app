99249a857ab3e6813279b032f5805b25168fee3b
t.mesropyan <Tatul.Mesropyan-extern@invia.de>
2022-08-15 00:44:55 +0400

    Search fields and request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import {Box, Button, Grid, TextField} from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState({})
+	const [city, setCity] = useState('');
+	const [country, setCountry] = useState('');
+	const location = `${city},${country}`
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=dc97e894c5d1389bd78904bb5cd1d0d3`
+
+	const getData = () => {
+		axios.get(url)
+			.then(res => {
+				const apiData = res.data;
+				setData({apiData});
+				setCity('')
+				setCountry('')
+			})
+			.catch(error => console.error('Alert data not found'))
+	}
+	useEffect(() => {
+		console.log(data)
+	}, [data])
+	return (
+		<div className='flex-container'>
+			<TextField value={city} fullWidth onChange={(e) => setCity(e.target.value)} label='City'/>
+			<TextField value={country} fullWidth onChange={(e) => setCountry(e.target.value)} label='Country'/>
+			<Button onClick={getData}>Submit</Button>
+		</div>
+	);
 }
 
 export default App;
